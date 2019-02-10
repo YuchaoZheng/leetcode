18536d635d9b31267591c2d5d3bf31d62e751691
YuchaoZheng <yuchao_zheng@163.com>
2019-02-10 13:12:24 +0800

    solve problem 44

--- 44. 通配符匹配.cpp
+++ 44. 通配符匹配.cpp
@@ -1,16 +1,18 @@
 //
-// Created by 郑宇超 on 2019/2/9.
+// Created by 郑宇超 on 2019/2/10.
 //
 /*
-给定一个字符串 (s) 和一个字符模式 (p)。实现支持 '.' 和 '*' 的正则表达式匹配。
+给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
 
-'.' 匹配任意单个字符。
-'*' 匹配零个或多个前面的元素。
-匹配应该覆盖整个字符串 (s) ，而不是部分字符串。
+'?' 可以匹配任何单个字符。
+'*' 可以匹配任意字符串（包括空字符串）。
+两个字符串完全匹配才算匹配成功。
 
 说明:
+
 s 可能为空，且只包含从 a-z 的小写字母。
-p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+p 可能为空，且只包含从 a-z 的小写字母，以及字符 ? 和 *。
+
 
 思路：
 先将p中连续的*转化为一个*
@@ -23,39 +25,31 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         vector <vector<bool>> dp(2, vector<bool>(s.size() + 1, false));
-        vector<bool> isSymbol(p.size() + 1, false);
         string copyP = "a";
         string copyS = "a" + s;
 
-        int copyPsize = 1;
         for (int i = 0; p[i] != '\0'; i++) {
             if (i > 0 && p[i - 1] == '*' && p[i] == '*')
                 continue;
-            if (p[i] == '*')
-                isSymbol[copyPsize - 1] = true;
-            else {
-                copyP += p[i];
-                copyPsize++;
-            }
+            copyP += p[i];
         }
 
         dp[0][0] = true;
         int pre = 0;
         for (int i = 1; copyP[i] != '\0'; i++) {
             for (int j = 1;; j++) {
-                if (isSymbol[i]) {
+                if (copyP[i] == '*') {
                     //不替换*
                     if (!dp[pre ^ 1][j - 1])
                         dp[pre ^ 1][j - 1] = dp[pre][j - 1];
                     // 替换*
-                    char preChar = copyP[i];
-                    if ((preChar == copyS[j] || preChar == '.') && dp[pre][j - 1] == true)
-                        while ((preChar == copyS[j] || preChar == '.') && copyS[j] != '\0') {
+                    if (dp[pre][j - 1] == true)
+                        while (copyS[j] != '\0') {
                             dp[pre ^ 1][j] = true;
                             j++;
                         }
                 } else {
-                    if ((copyP[i] == copyS[j] || copyP[i] == '.') && dp[pre][j - 1] == true)
+                    if ((copyP[i] == copyS[j] || copyP[i] == '?') && dp[pre][j - 1] == true)
                         dp[pre ^ 1][j] = true;
                 }
                 if (copyS[j] == '\0')
